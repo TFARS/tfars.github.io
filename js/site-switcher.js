658ae1f0d4403728a0e908b4a6b6f65515a8b1cc
lyofcs <lyofcs@sina.com>
2026-05-04 17:47:30 +0800

    更新网页切换按钮样式

--- js/site-switcher.js
+++ js/site-switcher.js
@@ -7,14 +7,14 @@
         const btn = document.createElement('div');
         btn.id = 'site-switch-btn';
 
-        const fallbackText = isCCL ? '天格会' : '高校联赛';
+        const fallbackText = isCCL ? 'TFA<br>积分榜' : 'CCL<br>积分榜';
         const imgSrc = isCCL ? '/img/btn_tfa.png' : '/img/btn_ccl.png';
 
         // 尝试图片，失败则显示文字
         const img = new Image();
         img.src = imgSrc;
         img.onerror = function () {
-            btn.textContent = fallbackText;
+            btn.innerHTML = fallbackText;
         };
         img.onload = function () {
             btn.appendChild(img);
